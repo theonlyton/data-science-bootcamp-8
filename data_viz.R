d874f37df20e701b59a0c5cee8a07a89d914d59e
theonlyton <acetony90@gmail.com>
2023-11-02 08:19:39 +0000

    Add new file

---
+++ data_viz.R
@@ -0,0 +1,8 @@
+# load ggplot2
+# update this file
+
+install.packages("ggplot2")
+library(ggplot2)
+
+ggplot(diamonds, aes(carat, price)) +
+  geom_point()
--- hello_world.r
+++ hello_world.r
@@ -6,3 +6,7 @@ library(dplyr)
 mtcars %>%
   select(1:5) %>%
   filter(mpg > 30)
+
+mtcars %>% 
+  select(mpg) %>% 
+  filter(mpg < 30)
